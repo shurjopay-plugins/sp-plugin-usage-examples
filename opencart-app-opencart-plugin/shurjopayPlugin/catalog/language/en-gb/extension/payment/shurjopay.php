33e79839f3f6acc9df3e0ca0f7339ec56c578854
Shahadat <shahadat@shurjomukhisolutions.com.bd>
2022-11-07 16:24:29 +0600

    opencart folder deleted

--- opencart-app-opencart-plugin/shurjopayPlugin/catalog/language/en-gb/extension/payment/shurjopay.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$_['text_title'] = 'Credit Card / Debit Card / bKash / Rocket / Nagad (shurjoPay)';
-$_['text_success'] = 'Your Transaction is successful';
-$_['text_failed'] = 'Your Transaction is failed';
