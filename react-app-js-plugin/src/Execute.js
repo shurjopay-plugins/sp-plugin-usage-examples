5d9e20a395de5b537710dc81e4f3ff3c03e82a6d
Saiful Islam Shanto <116637833+shant00@users.noreply.github.com>
2022-12-13 13:01:20 +0600

    js plugin integration

---
+++ react-app-js-plugin/src/Execute.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import { makePayment } from "./Shurjopay.js";
+const Execute = () => {
+  const [inputs, setInputs] = useState({});
+  const [loading, setLoading] = useState(false)
+
+  const order_id = "sp315689";
+
+
+
+  //getting data from userForm
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  //payment function
+  async function payNow(){
+    setLoading(true);
+        const makePayment_details= await makePayment(order_id, inputs);
+          const { checkout_url } = makePayment_details;
+          if (checkout_url) {
+           
+            window.location.href = checkout_url;
+          } else {
+            setLoading(false);
+            alert("Something Wrong on Your Payment");
+           
+          }
+        }
+  return (
+    <div>
+    
+    <div>
+      <form onChange={handleChange} id="form">
+        <div align="center" className="divlogo">
+          <img
+            className="logo"
+            src="https://d298h3gigxsryg.cloudfront.net/exhibitors/dUJ4EZNavnCYAn942NzQRVaQWbWk6gloHQtSr10a.png"
+            alt=""
+          />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="amount">
+            {" "}
+            Amount<span className="text-danger">*</span>
+          </label>
+          <input type="number" placeholder="Amount" name="amount" required />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="customer_name">
+            Customer Name<span className="text-danger">*</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Customer Name"
+            name="customer_name"
+            required
+          />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="customer_phone">
+            Customer Phone No<span className="text-danger">*</span>
+          </label>
+          <input
+            type="tel"
+            placeholder="Mobile Number"
+            name="customer_phone"
+            required
+          />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="customer_city">
+            Customer City<span className="text-danger">*</span>
+          </label>
+          <input type="text" placeholder="City" name="customer_city" required />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="customer_address">
+            Customer Address<span className="text-danger">*</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Address"
+            name="customer_address"
+            required
+          />
+        </div>
+        <div className="container-form-userName container-form-input">
+          <label htmlFor="customer_post_code">
+            Postal Code<span className="text-danger">*</span>
+          </label>
+          <input
+            type="number"
+            placeholder="Zip"
+            name="customer_post_code"
+            required
+          />
+        </div>
+        <div className="container-form-userPassword container-form-input">
+          <label htmlFor="customer_email">
+            Customer Email<span className="text-danger">*</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Email"
+            name="customer_email"
+            required
+          />
+        </div>
+        <div className="container-form-userPassword container-form-input">
+          <label htmlFor="currency">
+            Currency<span className="text-danger">*</span>
+          </label>
+          <select name="currency" required>
+            <option value>Please Select One</option>
+            <option value="BDT">BDT</option>
+            <option value="USD">USD</option>
+          </select>
+        </div>
+      </form>
+      <div align="center">
+        {/* className="grid-container" */}
+        <button
+          className="spButton  js-form-btn"
+          onClick={() => payNow()}
+          id="payButton"
+        >{loading ? (
+          <div className="loader-container">
+            <div className="loader"></div>
+          </div>
+        ) : ""}
+          Pay Now
+        </button>
+        {/* <button
+        className="spButton  js-form-btn dbutton"
+      >
+       Default
+      </button> */}
+      </div>
+    </div>
+   
+    </div>
+  );
+};
+
+export default Execute;
---
+++ react-app-js-plugin/src/Return.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { verifyPayment } from "./Shurjopay.js";
+
+const Return = () => {
+  const [details, setDetails] = useState({});
+  async function status() {
+    //getting order id from url
+    const url_string = window.location.href.toLowerCase();
+    const url = new URL(url_string);
+    const order_id = url.searchParams.get("order_id");
+    
+     
+      //payment verification and handle promise data 
+      //parameter order_id to call verifyPayment function
+      const verify_status= await verifyPayment(order_id);
+   
+        for (const values of verify_status) {
+          setDetails(values);
+        }
+  }
+  window.onload = () => status();
+  return (
+    <div>
+      <h1 align="center">Payment Slip</h1>
+      <table id="customers">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>{details.name}</td>
+          </tr>
+          <tr>
+            <td>Address</td>
+            <td>{details.address}</td>
+          </tr>
+          <tr>
+            <td>Order Id:</td>
+            <td>{details.customer_order_id}</td>
+          </tr>
+          <tr>
+            <td>Currency</td>
+            <td>{details.currency}</td>
+          </tr>
+          <tr>
+            <td>Discount Percentage:</td>
+            <td>{details.disc_percent}</td>
+          </tr>
+          <tr>
+            <td>Amount:</td>
+            <td>{details.amount}</td>
+          </tr>
+          <tr>
+            <td>Status:</td>
+            <td>{details.sp_message}</td>
+          </tr>
+          <tr>
+            <td>Payment Method</td>
+            <td>{details.method}</td>
+          </tr>
+          <tr>
+            <td>Transaction ID:</td>
+            <td>{details.bank_trx_id}</td>
+          </tr>
+          <tr>
+            <td>Invoice No</td>
+            <td>{details.invoice_no}</td>
+          </tr>
+          <tr>
+            <td>Date &amp; Time:</td>
+            <td>{details.date_time}</td>
+          </tr>
+        </tbody>
+      </table>
+      <br />{" "}
+      <div align="center">
+        <Link to="/payment"> Back to Home </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Return;
