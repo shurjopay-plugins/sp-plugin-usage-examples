94bcee994d4369bdf2f950e4b016f79acf106b3b
Rayhan-khan-Ridoy <kredoy415@gmail.com>
2022-12-14 12:08:41 +0600

    This usage project using sp-plugin-laravel plugin from git repo

--- laravel-app-laravel-plugin/shurjopay_integ_updated_usage_project/packages/Shurjomukhi/ShurjopayLaravelPlugin/src/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Shurjomukhi\ShurjopayLaravelPlugin\Http\Controllers\Shurjopay;
-
-Route::get('/test',[Shurjopay::class,'authenticate']);
-Route::post('/test2',[Shurjopay::class,'makePayment']);
-Route::post('/test3',[Shurjopay::class,'verifyPayment']);
