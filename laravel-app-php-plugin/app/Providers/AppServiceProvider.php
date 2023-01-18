ff0ed3ea7dd9ded3326c7319ff3b9333c3a3f457
RAYHAN KHAN RIDOY <78033774+Rayhan-khan-Ridoy@users.noreply.github.com>
2023-01-18 18:29:43 +0600

    Update AppServiceProvider.php

--- laravel-app-php-plugin/app/Providers/AppServiceProvider.php
+++ laravel-app-php-plugin/app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//        $this->app->make('ShurjopayPlugin\Shurjopay');
+
     }
 PHP Parse error:  syntax error, unexpected 'public' (T_PUBLIC), expecting end of file in HelloWorld.php on line 2
 
