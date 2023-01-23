7b7b00acc9d5bef2b9d7fd009754e9aacb4c01b8
filelucker <filelucker@gmail.com>
2023-01-23 15:19:36 +0600

    android example added

---
+++ android-app-kotlin-plugin/app/src/main/java/com/shurjopay/android/example/utils/Constants.kt
@@ -0,0 +1,15 @@
+package com.shurjopay.android.example.utils
+
+import androidx.annotation.Keep
+
+@Keep
+class Constants {
+    @Keep
+    companion object {
+        // shujopay configuration parameter
+        const val SP_USER = "sp_sandbox"
+        const val SP_PASS = "pyyk97hu&6u6"
+        const val SHURJOPAY_API = "https://sandbox.shurjopayment.com/api/"
+    }
+
+}
