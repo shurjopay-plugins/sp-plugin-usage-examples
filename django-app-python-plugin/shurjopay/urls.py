5af8cc559181bca57d0108f2de0d299d3d492f7c
mahabubul470 <mahabubul470@gmail.com>
2023-03-23 16:26:59 +0600

    Add admin site urls

--- django-app-python-plugin/shurjopay/urls.py
+++ django-app-python-plugin/shurjopay/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
+from django.contrib import admin
 
-from . import views
+from shurjopay import views
 
 urlpatterns = [
+    path ('admin/', admin.site.urls),
     path('', views.index, name='shurjopay'),
     path('make-sp-payment', views.make_payment, name='make-sp-payment'),
-    
     path('ipn', views.ipn,name='ipn'),
     path('return', views.sp_return,name='return'),
     path('cancel', views.sp_cancel,name='cancel'),
     path('check-payment', views.check_payment_view),
-    path('check-sp-payment', views.check_payment,name='check-sp-payment'),
 ]
