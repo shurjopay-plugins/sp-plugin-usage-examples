5b1571528bf78720aacc4c2b69ef082276cb7ad6
Imtiaz Rahi <imtiaz.rahi@gmail.com>
2023-04-24 13:15:57 +0600

    Getting DB_FILE path from config/env

--- node-app-node-plugin/dbConnect.js
+++ node-app-node-plugin/dbConnect.js
@@ -1,11 +1,12 @@
 // Importing required packages and modules
 const { Sequelize } = require("sequelize");
 require("sqlite3").verbose();
+require("dotenv").config();
 
 //creates a new Sequelize instance with the dialect set to "sqlite" and the storage option set to "./sp.db"
 const dbConnect = new Sequelize({
   dialect: "sqlite",
-  storage: "./sp.db",
+  storage: process.env.DB_FILE,
   logging: false,
 });
 
